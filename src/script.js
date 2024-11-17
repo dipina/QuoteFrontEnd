5283429416d239a1edb56b4b7b43cd2015efe26a
dipina <dipina@deusto.es>
2024-11-17 12:10:34 +0100

    turned front end into simple html with javascript app which calls to API gateway, whose location is hardcoded in script.js

--- src/script.js
+++ src/script.js
@@ -1,48 +1,50 @@
-var api = process.env.API_GATEWAY; // get the API Gateway from the environment
+// Define the API Gateway constant
+const API_GATEWAY = "https://apigateway-railways-production.up.railway.app";
 
 $(document).ready(function() {
     $("#btn").click(function() {
         $.ajax({
-            url: api + "/api/randomquote",
+            url: API_GATEWAY + "/api/randomquote",
             type: "GET",
             dataType: "json",
             timeout: 3000,
             success: function(data) {
-                $("#quote").removeClass('is-danger') 
-                $("#quote").addClass('is-link')
-                $( "#quote" ).html(data.quote.quote + '</br><b>'+ data.quote.by +'</b>'); 
+                $("#quote").removeClass('is-danger');
+                $("#quote").addClass('is-link');
+                $("#quote").html(data.quote.quote + '</br><b>' + data.quote.by + '</b>');
             },
             error: function(xmlhttprequest, textstatus, message) {
-                $("#quote").removeClass('is-link')
-                $("#quote").addClass('is-danger')
-                if(textstatus==="timeout") {
-                    $( "#quote" ).html("got timeout");
+                $("#quote").removeClass('is-link');
+                $("#quote").addClass('is-danger');
+                if (textstatus === "timeout") {
+                    $("#quote").html("got timeout");
                 } else {
-                    $( "#quote" ).html(message);
+                    $("#quote").html(message);
                 }
             }
-        })
+        });
     });
-	$("#btn-status").click(function() {
+
+    $("#btn-status").click(function() {
         $.ajax({
-            url: api + "/api/status",
+            url: API_GATEWAY + "/api/status",
             type: "GET",
             dataType: "json",
             timeout: 3000,
             success: function(data) {
-                $("#status").removeClass('is-danger') 
-                $("#status").addClass('is-link')
-                $("#status" ).html(data.status); 
+                $("#status").removeClass('is-danger');
+                $("#status").addClass('is-link');
+                $("#status").html(data.status);
             },
             error: function(xmlhttprequest, textstatus, message) {
-                $("#status").removeClass('is-link')
-                $("#status").addClass('is-danger')
-                if(textstatus==="timeout") {
-                    $( "#status" ).html("got timeout");
+                $("#status").removeClass('is-link');
+                $("#status").addClass('is-danger');
+                if (textstatus === "timeout") {
+                    $("#status").html("got timeout");
                 } else {
-                    $( "#status" ).html(message);
+                    $("#status").html(message);
                 }
             }
-        })
+        });
     });
-})
+});
